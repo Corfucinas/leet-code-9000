cd251ee741e5d5500c1556f3c80e6462ab01a318
Corfucinas <corfucinas@protonmail.com>
2020-07-08 17:45:43 +0800

    reverse integer

---
+++ src/leet_code_9000/7.reverse-integer.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#
+# @lc app=leetcode id=7 lang=python3
+#
+# [7] Reverse Integer
+#
+
+# @lc code=start
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x >= 2 ** 31 - 1 or x <= -(2 ** 31):
+            return 0
+        else:
+            if x >= 0:
+                string = str(x)
+                reverse_string = string[::-1]
+            else:
+                temp = string[1:]
+                temp2 = temp[::-1]
+                reverse_string = '-' + temp2
+            if int(reverse_string) >= 2 ** 31 - 1 or int(reverse_string) <= -(
+                2 ** 31
+            ):
+                return 0
+            else:
+                return int(reverse_string)
+
+
+# @lc code=end
