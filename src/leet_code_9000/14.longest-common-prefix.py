e0a9914eaa04402f4c38ff3eeda72626771e77a3
Corfucinas <corfucinas@protonmail.com>
2020-07-09 12:42:57 +0800

    add longest-common-prefix

---
+++ src/leet_code_9000/14.longest-common-prefix.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# @lc app=leetcode id=14 lang=python3
+#
+# [14] Longest Common Prefix
+#
+
+# @lc code=start
+
+from typing import List
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ''
+        prefix = strs[0]
+        for string in strs[1:]:
+            while string[:len(prefix)] != prefix and prefix:
+                prefix = prefix[:len(prefix)-1]
+            if not prefix:
+                ''
+        res = prefix
+        return res
+
+# @lc code=end
