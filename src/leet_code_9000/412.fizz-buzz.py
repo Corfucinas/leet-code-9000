5fc1a26aba3cb741d3e3625e84192b0b43a5ad04
Corfucinas <corfucinas@protonmail.com>
2020-07-09 17:51:35 +0800

    add fizz-buzz

---
+++ src/leet_code_9000/412.fizz-buzz.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# @lc app=leetcode id=412 lang=python3
+#
+# [412] Fizz Buzz
+#
+
+# @lc code=start
+
+from typing import List
+
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        result = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                result.append('FizzBuzz')
+            elif i % 3 == 0:
+                result.append('Fizz')
+            elif i % 5 == 0:
+                result.append('Buzz')
+            else:
+                result.append(str(i))
+        return result
+
+
+# @lc code=end
