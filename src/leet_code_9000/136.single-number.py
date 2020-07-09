8973543982e807f2189ebd021cdd9f07501ab049
Corfucinas <corfucinas@protonmail.com>
2020-07-09 16:59:48 +0800

    add single-number

---
+++ src/leet_code_9000/136.single-number.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# @lc app=leetcode id=136 lang=python3
+#
+# [136] Single Number
+#
+
+# @lc code=start
+
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        for unique in nums:
+            if nums.count(unique) == 1:
+                return unique
+
+
+# @lc code=end
