70462fdffd783bde472eb500382ad8f44861395e
Corfucinas <corfucinas@protonmail.com>
2020-07-09 17:10:25 +0800

    add reverse-string

---
+++ src/leet_code_9000/344.reverse-string.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# @lc app=leetcode id=344 lang=python3
+#
+# [344] Reverse String
+#
+
+# @lc code=start
+
+from typing import List
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
+
+
+# @lc code=end
