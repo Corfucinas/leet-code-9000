bb1b58303c42c075278aa616237a7f1b40f215ad
Corfucinas <corfucinas@protonmail.com>
2020-07-10 14:41:03 +0800

    add maximum-69-number

---
+++ src/leet_code_9000/1323.maximum-69-number.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# @lc app=leetcode id=1323 lang=python3
+#
+# [1323] Maximum 69 Number
+#
+
+# @lc code=start
+class Solution:
+    def maximum69Number(self, num: int) -> int:
+        return int(str(num).replace('6', '9', 1))
+
+
+# @lc code=end
