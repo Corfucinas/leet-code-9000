ff6194956b82d80821d9ef6c86de69f166d8f18d
Corfucinas <corfucinas@protonmail.com>
2020-07-13 17:05:31 +0800

    add search-insert-position

---
+++ src/leet_code_9000/35.search-insert-position.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# @lc app=leetcode id=35 lang=python3
+#
+# [35] Search Insert Position
+#
+
+# @lc code=start
+
+from typing import List
+
+
+class Solution:
+    def searchInsert(
+        self, nums: List[int], target: int
+    ) -> int:  #  type: ignore
+        for i in nums:
+            if i == target or target < i:
+                return nums.index(i)
+            elif target not in nums:
+                nums.append(target)
+                return sorted(nums).index(target)
+
+
+# @lc code=end
