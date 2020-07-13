13c4f7eff163e7e23f8f2cdb96a22887cd810f4d
Corfucinas <corfucinas@protonmail.com>
2020-07-13 15:48:05 +0800

    add sqrt-x

---
+++ src/leet_code_9000/69.sqrt-x.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# @lc app=leetcode id=69 lang=python3
+#
+# [69] Sqrt(x)
+#
+
+# @lc code=start
+
+from math import sqrt
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return int(sqrt(x))
+
+
+# @lc code=end
