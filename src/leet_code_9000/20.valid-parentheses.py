661dd1a7b8756efda47eebfe37ac8a6969654871
Corfucinas <corfucinas@protonmail.com>
2020-07-13 16:38:58 +0800

    add valid-parentheses

---
+++ src/leet_code_9000/20.valid-parentheses.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# @lc app=leetcode id=20 lang=python3
+#
+# [20] Valid Parentheses
+#
+
+# @lc code=start
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = {'(': ')', '[': ']', '{': '}'}
+        valid = []
+        for item in s:
+            if item in brackets:
+                valid.append(brackets[item])
+            else:
+                if not (valid and valid.pop() == item):
+                    return False
+        return not valid
+
+
+# @lc code=end
