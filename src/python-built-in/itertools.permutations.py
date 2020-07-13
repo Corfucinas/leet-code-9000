774ec01776990d0d2cbf3351e47aec0e1b0a927d
Corfucinas <53630064+Corfucinas@users.noreply.github.com>
2020-07-13 17:48:03 +0800

    add itertools.permutations

--- src/python-built-in/itertools.permutations.py
+++ src/python-built-in/itertools.permutations.py
@@ -8,13 +8,13 @@ def permutations(iterable, r=None):
     if r > n:
         return
     indices = list(range(n))
-    cycles = list(range(n, n-r, -1))
+    cycles = list(range(n, n - r, -1))
     yield tuple(pool[i] for i in indices[:r])
     while n:
         for i in reversed(range(r)):
             cycles[i] -= 1
             if cycles[i] == 0:
-                indices[i:] = indices[i+1:] + indices[i:i+1]
+                indices[i:] = indices[i + 1 :] + indices[i : i + 1]
                 cycles[i] = n - i
             else:
                 j = cycles[i]
