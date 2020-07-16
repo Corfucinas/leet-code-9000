0319b497dad26f130962e09369c56357eb659f9e
Corfucinas <53630064+Corfucinas@users.noreply.github.com>
2020-07-16 16:40:35 +0800

    add best-time-to-buy-and-sell-stock

---
+++ src/leet_code_9000/121.best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# @lc app=leetcode id=121 lang=python3
+#
+# [121] Best Time to Buy and Sell Stock
+#
+
+# @lc code=start
+
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+        profit, stock = float('-inf'), prices[0]
+        for p in prices:
+            profit = max(profit, p - stock)
+            stock = min(stock, p)
+        return int(profit)
+
+
+# @lc code=end
