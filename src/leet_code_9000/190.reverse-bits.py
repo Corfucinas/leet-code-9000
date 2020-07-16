10e10e9eea4b34080678f0973bdf57c48e1089c2
Corfucinas <53630064+Corfucinas@users.noreply.github.com>
2020-07-16 17:43:38 +0800

    reverse-bits

---
+++ src/leet_code_9000/190.reverse-bits.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# @lc app=leetcode id=190 lang=python3
+#
+# [190] Reverse Bits
+#
+
+# @lc code=start
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        ret, power = 0, 31
+        while n:
+            ret += (n & 1) << power
+            n >>= 1
+            power -= 1
+        return ret
+
+
+# @lc code=end
