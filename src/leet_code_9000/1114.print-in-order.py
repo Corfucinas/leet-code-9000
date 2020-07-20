e96b89221a946aff42f39c6213d30ef3cefd7dc7
Corfucinas <53630064+Corfucinas@users.noreply.github.com>
2020-07-20 15:05:57 +0800

    add print-in-order

---
+++ src/leet_code_9000/1114.print-in-order.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#
+# @lc app=leetcode id=1114 lang=python3
+#
+# [1114] Print in Order
+#
+
+# @lc code=start
+
+from threading import Semaphore
+from typing import Callable
+
+
+class Foo:
+    def __init__(self):
+        self.second_sem = Semaphore(1)
+        self.third_sem = Semaphore(1)
+        self.second_sem.acquire()
+        self.third_sem.acquire()
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        self.second_sem.release()
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        self.second_sem.acquire()
+        # printSecond() outputs "second". Do not change or remove this line.
+        printSecond()
+        self.third_sem.release()
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        self.third_sem.acquire()
+        # printThird() outputs "third". Do not change or remove this line.
+        printThird()
+
+
+# @lc code=end
